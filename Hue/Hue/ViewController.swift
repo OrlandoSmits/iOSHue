5bac2c30a4df2eb962ce4a89b77f5b2b0db6bcd2
Orlando Smits <ojasmits@pc131361.lan>
2016-10-24 11:59:05 +0200

    Initial Commit

---
+++ Hue/Hue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hue
+//
+//  Created by Orlando Smits on 24/10/2016.
+//  Copyright © 2016 Orlando Smits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
