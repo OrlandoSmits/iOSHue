9c488859c7e631555a3db6be7fedb4b1db9e9076
Orlando Smits <orlandosmits@live.nl>
2016-10-25 17:01:27 +0200

    Omdat we nooit gaan begrijpen hoe je nu door deze JSON heen loopt met kut Swift

--- Hue/Hue/HueTableViewController.swift
+++ Hue/Hue/HueTableViewController.swift
@@ -14,7 +14,27 @@ class HueTableViewController: UITableViewController {
     
     var hues: [Hue] = []
     
-    
+    func getState(par: String)
+    {
+
+        let url = "http://192.168.2.27:80/api/newdeveloper/lights/\(par)/state"
+        Alamofire.request(url,
+                          method: .get,
+                          encoding: URLEncoding.default).responseJSON { (responseData) -> Void in
+                            if((responseData.result.value) != nil) {
+                                let results = JSON(responseData.result.value!)
+                                for(key, result) in results["lights"] {
+                                    for light in result {
+//                                        let on = light["state"]["on"].bool                                  
+                                    }
+                                
+                                }
+                            }
+                            
+        }
+        
+
+    }
     func getHues() {
         
 //        let url = "http://192.168.1.179/api/80b8a9620291a47fec92fa34484f5b/lights"
@@ -32,13 +52,13 @@ class HueTableViewController: UITableViewController {
                                     let hue = Hue()
                                     let id = Int(key)
                                     
-                                    for subJson in result["state"] {
-                                        print(subJson["bri"].stringValue)
-                
-                                    }
+                                    let state = self.getState(par: key)
+//                                    print(state)
+                                    
                                         
                             
-                                    let on = result["on"].bool
+                                    let on = result["state"]["on"].bool
+                                    print(on)
 //                                    let bri = result["bri"].int
                                     
                                     hue.id = id!
